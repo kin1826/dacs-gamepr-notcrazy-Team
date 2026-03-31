1cf26d2a99a421f9c9b881915fcb343612a0074e
kin1826 <tvbphu2@gmail.com>
2026-03-31 13:16:47 +0700

    fect(all) commit first

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 8f;
+    public float jumpForce = 5f;
+    public float gravity = 2f;
+    
+    [Header("Ground Check")]
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    [SerializeField] private Animator animator; // kéo Graphics vào đây
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Input System gọi
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    
+    // 🦘 NHẢY
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void Update()
+    {
+        // 🧱 CHECK ĐẤT
+        isGrounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundRadius,
+            groundLayer
+        );
+        Debug.Log(isGrounded);
+        
+        // 🎭 Animation
+        float speed = Mathf.Abs(moveInput.x);
+
+        bool isRunning = speed > 0.1f;
+
+        // chống tụt về 0 đột ngột
+        if (speed < 0.3f && Mathf.Abs(rb.linearVelocity.x) > 0.3f)
+        {
+            isRunning = true;
+        }
+        
+        // bool isRunning = Mathf.Abs(moveInput.x) > 0.1f;
+        animator.SetBool("isRunning", isRunning);
+
+        // 🔁 Flip
+        if (moveInput.x != 0)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = Mathf.Sign(moveInput.x) * Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
+    }
+}
