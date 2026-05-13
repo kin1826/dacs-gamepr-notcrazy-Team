76e8bef29abd595348d8be52cf59d39f3f3ee31b
kin1826 <tvbphu2@gmail.com>
2026-05-14 01:01:39 +0700

    fect: done local

---
+++ Assets/Script/core/RoomData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public static class RoomData
+{
+    public static int HostDino = -1;
+    public static int ClientDino = -1;
+
+    public static string RoomCode = "";
+
+    // public static void SelectHostDino(int index)
+    // {
+    //     RoomData.HostDino = index;
+
+    //     hostDinoImage.sprite = dinoSprites[index];
+
+    //     createButton.interactable = true;
+    // }
+}
--- Assets/Script/manager/MainManager.cs
+++ Assets/Script/manager/MainManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainManager : MonoBehaviour
 {
@@ -10,6 +12,40 @@ public class MainManager : MonoBehaviour
 
     [Header("Scene")]
     public string gameplaySceneName = "Level_1";
+
+    [Header("Dino Sprites")]
+    public Sprite[] dinoSprites;
+
+    [Header("Room Images")]
+    public Image leftDinoImage;
+    public Image rightDinoImage;
+
+    [Header("Buttons")]
+    public Button createCodeButton;
+    public Button joinRoomButton;
+    public Button playButton;
+
+    [Header("Dino Buttons")]
+    public Button[] dinoButtons;
+
+    [Header("Join")]
+    public TMP_InputField roomCodeInput;
+    public TMP_InputField codeDisplayInput;
+
+    private void Start()
+    {
+        createCodeButton.interactable = false;
+        joinRoomButton.interactable = true;
+        playButton.interactable = false;
+
+        leftDinoImage.gameObject.SetActive(false);
+        rightDinoImage.gameObject.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameplaySceneName);
+    }
 
     // =========================
     // SINGLE PLAYER
@@ -40,29 +76,70 @@ public class MainManager : MonoBehaviour
     {
         GameData.IsHost = true;
 
-        multiplayerPanel.SetActive(false);
         dinoSelectPanel.SetActive(true);
     }
 
     // =========================
     // JOIN ROOM
     // =========================
-    public void JoinRoom()
-    {
-        GameData.IsHost = false;
+    // public void JoinRoom()
+    // {
+    //     GameData.IsHost = false;
 
-        multiplayerPanel.SetActive(false);
+    //     multiplayerPanel.SetActive(false);
+    //     dinoSelectPanel.SetActive(true);
+    // }
+
+    public void OpenDinoSelect()
+    {
         dinoSelectPanel.SetActive(true);
     }
 
     // =========================
     // SELECT DINO
     // =========================
-    public void SelectDino(int dinoIndex)
+    public void SelectDino(int index)
     {
-        GameData.SelectedDino = dinoIndex;
+        // SINGLE PLAYER
+        if (!GameData.IsMultiplayer)
+        {
+            GameData.SelectedDino = index;
 
-        SceneManager.LoadScene(gameplaySceneName);
+            SceneManager.LoadScene(gameplaySceneName);
+
+            return;
+        }
+
+        // MULTIPLAYER HOST
+        if (GameData.IsHost)
+        {
+            RoomData.HostDino = index;
+
+            leftDinoImage.sprite = dinoSprites[index];
+            leftDinoImage.gameObject.SetActive(true);
+
+            dinoSelectPanel.SetActive(false);
+
+            createCodeButton.interactable = true;
+
+            Debug.Log("HOST SELECT DINO");
+        }
+        else
+        {
+            // MULTIPLAYER CLIENT
+            RoomData.ClientDino = index;
+
+            rightDinoImage.sprite = dinoSprites[index];
+            rightDinoImage.gameObject.SetActive(true);
+
+            dinoSelectPanel.SetActive(false);
+
+            playButton.interactable = true;
+
+            Debug.Log("CLIENT SELECT DINO");
+
+            UpdateRoomUI();
+        }
     }
 
     // =========================
@@ -123,5 +200,72 @@ public class MainManager : MonoBehaviour
         Debug.Log("QUIT GAME");
 
         Application.Quit();
+    }
+
+    public void CreateRoomCode()
+    {
+        RoomData.RoomCode = Random.Range(1000, 9999).ToString();
+
+        codeDisplayInput.text = RoomData.RoomCode;
+
+        playButton.interactable = false;
+
+        Debug.Log("ROOM CODE: " + RoomData.RoomCode);
+    }
+
+    public void JoinRoom()
+    {
+        string inputCode = roomCodeInput.text;
+
+        GameData.IsHost = false;
+
+        dinoSelectPanel.SetActive(true);
+
+        DisableHostDino();
+
+        Debug.Log("JOIN SUCCESS");
+
+        // if (inputCode == RoomData.RoomCode)
+        // {
+        //     GameData.IsHost = false;
+
+        //     dinoSelectPanel.SetActive(true);
+
+        //     DisableHostDino();
+
+        //     Debug.Log("JOIN SUCCESS");
+        // }
+        // else
+        // {
+        //     Debug.Log("WRONG CODE");
+        // }
+    }
+
+    private void DisableHostDino()
+    {
+        int hostIndex = RoomData.HostDino;
+
+        dinoButtons[hostIndex].interactable = false;
+    }
+
+    public void UpdateRoomUI()
+    {
+        if (RoomData.HostDino != -1)
+        {
+            leftDinoImage.sprite =
+                dinoSprites[RoomData.HostDino];
+
+            leftDinoImage.gameObject.SetActive(true);
+        }
+
+        if (RoomData.ClientDino != -1)
+        {
+            rightDinoImage.sprite =
+                dinoSprites[RoomData.ClientDino];
+
+            rightDinoImage.gameObject.SetActive(true);
+
+            playButton.interactable = true;
+        }
     }
 }
